0ea53cbd8737b856e65eca7c6349208362a477c3
SeYeol00 <dmot0126@gmail.com>
2023-08-13 21:28:03 +0900

    Initial commit

---
+++ src/main/kotlin/com/coroutine/coroutinepractice/CoroutinePracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.coroutine.coroutinepractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutinePracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoroutinePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/coroutine/coroutinepractice/CoroutinePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coroutine.coroutinepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutinePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
